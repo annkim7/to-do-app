317c4520d60e98a55dcced4e9a318c53c5335046
annkim7 <ilc7748@gmail.com>
2023-01-18 16:27:14 +0900

    #2 Item.js 리스트 아이콘 스타일 추가

--- to-do-react/src/pages/Main.js
+++ to-do-react/src/pages/Main.js
@@ -1,12 +1,19 @@
 import React from "react";
+
+import styled from "styled-components";
+
 import Notice from "../components/Notice";
 import Item from "../components/Item";
+
+const MatinTitle = styled.h3`
+  margin-bottom: 1.438rem;
+`;
 
 export default function Main() {
   return (
     <article>
       <Notice />
-      <h3 className="mainTitle">할일 목록</h3>
+      <MatinTitle>할일 목록</MatinTitle>
       <ul>
         <Item></Item>
       </ul>
--- to-do-react/src/styles/Global.js
+++ to-do-react/src/styles/Global.js
@@ -8,6 +8,7 @@ const GlobalStyle = createGlobalStyle`
     }
     html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, menu, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, main, menu, nav, output, ruby, section, summary, time, mark, audio, video {
         font-family: 'Noto Sans KR', sans-serif;
+        font-size:100%;
     }
     a{
         text-decoration: none;
@@ -34,8 +35,19 @@ const GlobalStyle = createGlobalStyle`
         cursor: pointer;
     }
 
+    html{
+        overflow:hidden;
+        height: 100%;
+    }
+    body{
+        ${({ theme }) => theme.flexBox("row", "center", "center")}
+        height:100%;
+        background:#f5f5f5;
+    }
+
     #root{
-        max-width: 400px;
+        width: 400px;
+        height:800px;
         margin: 0 auto;
         border:1px solid #ddd;
         border-radius: 1rem;
@@ -51,8 +63,7 @@ const GlobalStyle = createGlobalStyle`
         padding-top:0;
     }
 
-    .mainTitle{
-        margin-bottom:1.438rem;
+    h3{
         font-size:1rem;
         font-weight:600;
         letter-spacing: -0.001em;
